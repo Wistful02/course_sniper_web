4fdcc17fb9af26cabc24531600ddc6187bc62d90
Wistful02 <70364434+Wistful02@users.noreply.github.com>
2023-07-13 16:56:29 +0800

    Update sessions_status_updater.py

--- src/sessions_status_updater.py
+++ src/sessions_status_updater.py
@@ -6,7 +6,7 @@ import json
 load_dotenv()
 
 
-def main():
+def return_data():
 
     indexArray=[]
     with open(os.getenv('INDEX_FILE_TXT'),mode='r') as f:
@@ -15,20 +15,23 @@ def main():
     url="http://sis.rutgers.edu/soc/api/openSections.gzip"
     info={('year',str(2023)),('term',str(9)),('campus','NB')} #year=year, term= fall'9',spring'1'
     while True:
-        print('-----')
+        #print('-----')
         subjects = requests.get(url,params=info)
         subjects.raise_for_status()
         subjects=subjects.json()
         openClasses=[]
-        print(subjects)
         for index in indexArray:
             for item in subjects:
                 if item==index:
                     openClasses.append(index)
                     break
 
+        
+        ret = []
         for index in openClasses:
-            print(index+' is open!')
+            ret.append(index+' is open!')
+
+        print(f"ssu is running... length of arr:{len(ret)}")
         '''
         registerCommand='./webregBot.py'
         for index in openClasses:
@@ -40,8 +43,6 @@ def main():
                     if index==i:
                         indexArray.remove(i)
         '''
-        print ('Sleeping...\nPress ctrl-c to stop.\n')
-        sleep(2)
-
-if __name__=='__main__':
-    main()
+        #print ('Sleeping...\nPress ctrl-c to stop.\n')
+        #sleep(2)
+        return ret
