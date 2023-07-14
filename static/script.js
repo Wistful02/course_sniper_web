6e1a94f88d9b1d4073cdbc60b37fedeed8211a1d
Wistful02 <70364434+Wistful02@users.noreply.github.com>
2023-07-14 10:32:01 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,26 +3,47 @@ var socket = io({
     port: 5000
 });
 
+var textArray = [];
+
+function createDeleteHandler(text) {
+    return function () {
+        textArray.push(text);
+        this.parentNode.remove();
+        console.log(textArray);
+    };
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
 
 socket.on('sex_with_andrewmama', function (message) {
     console.log("response received");
-    console.log(message);
 
     var messageList = document.getElementById('input_list');
     messageList.innerHTML = ''; // Clear the existing content
 
     for (var i = 0; i < message.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = message[i];
-        messageList.appendChild(listItem);
+        var text = message[i] + " is open!";
+
+        if (!textArray.includes(text)) {
+            var listItem = document.createElement('li');
+            listItem.textContent = text;
+
+            var deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.classList.add('delete-button');
+            deleteButton.addEventListener('click', createDeleteHandler(text));
+
+            listItem.appendChild(deleteButton);
+            messageList.appendChild(listItem);
+        }
     }
+    console.log(textArray)
 });
 
 document.getElementById('start_snipe_button').addEventListener('click', function () {
-    document.getElementById('status_bar').innerHTML = 'Starting Sniper...';
+    document.getElementById('status_bar').innerHTML = 'Starting Sniper... (This might take a while depending on the device you are using)';
     socket.emit('start_sniper');
     console.log('Starting_sniper')
 });
